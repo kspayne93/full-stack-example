2dfe4eb39c718cd47802dba1b66986ec37a28a51
kspayne93 <kyle@investavante.com>
2019-01-11 13:44:30 -0700

    beautified code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './communityBank.svg';
 import axios from 'axios';
 import './Login.css';
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2'; 
 
 export default class Login extends Component {
    constructor(props) {
@@ -25,30 +25,30 @@ export default class Login extends Component {
       const { email, password } = this.state;
       const res = await axios.post('/auth/login', { email: email, password: password })
       if (res.data.loggedIn) {
-          await Swal({ //await will make it so that the alert will pop up first before routing to the /private page
+         await Swal({ //await will make it so that the alert will pop up first before routing to the /private page
             type: 'success',
             title: 'Nice!',
             text: 'You have successfully logged in.'
-          })
+         })
          this.props.history.push('/private') //this sends the user to the private route
       }
    }
 
-  render() {
-    return (
-      <div>
-        <img src={logo} alt="" />
-        <p>
-           <span>Email:</span>
-           <input onChange={(e) => this.setState({ email: e.target.value })} type="text"/>
-        </p>
-        <p>
-           <span>Password:</span>
-           <input onChange={(e) => this.setState({ password: e.target.value })} type="text"/>
-        </p>
-        <button onClick={() => this.register()} >Register</button>
-        <button onClick={() => this.login()} >Login</button>
-      </div>
-    )
-  }
+   render() {
+      return (
+         <div>
+            <img src={logo} alt="" />
+            <p>
+               <span>Email:</span>
+               <input onChange={(e) => this.setState({ email: e.target.value })} type="text" />
+            </p>
+            <p>
+               <span>Password:</span>
+               <input onChange={(e) => this.setState({ password: e.target.value })} type="text" />
+            </p>
+            <button onClick={() => this.register()} >Register</button>
+            <button onClick={() => this.login()} >Login</button>
+         </div>
+      )
+   }
 }
