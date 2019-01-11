951f271ac301ffcad6b39c0f13a5a29a53ca21c9
kspayne93 <kyle@investavante.com>
2019-01-11 13:43:47 -0700

    added comments

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ massive(CONNECTION_STRING).then(db => {
    app.set('db', db);  //app.set is a method that sets values to an object on app. We're setting the db value to a key called 'db' on app. 
       console.log('Connected to database')
    app.listen(SERVER_PORT, () => { 
-     console.log(`Listening on port: ${SERVER_PORT}`) //putting the listen method in the .then ensures that the server won't watch for changes or update until after the database is connected
+      console.log(`Listening on port: ${SERVER_PORT}`) //putting the listen method in the .then ensures that the server won't watch for changes or update until after the database is connected
    })
  })
 
@@ -29,7 +29,7 @@ massive(CONNECTION_STRING).then(db => {
 app.post('/auth/register', authCtrl.register) //register
 app.post('/auth/login', authCtrl.login) //login
 app.get('/api/user-data', authCtrl.userData) //show user data
-app.get('/auth/logout', (req,res) => {
+app.get('/auth/logout', (req,res) => { //logout
    req.session.destroy();
    res.redirect('http://localhost:3000/#/')
 })
